1f035fa55148ac3f44d35fc70c2e6221fc06a20f
Marco Echevestre <marcoaminotto@hotmail.com>
2019-03-08 14:50:42 -0300

    Uploaded the project

---
+++ app.js
@@ -0,0 +1,67 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+//Customize yargs version
+yargs.version('1.1.0')
+
+//Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true, //make the title required
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body)
+  }
+})
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.removeNote(argv.title)
+  }
+})
+
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler() {
+    notes.listNotes()
+  }
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.readNote(argv.title)
+  }
+})
+
+yargs.parse() //print informations when u do console.log();
